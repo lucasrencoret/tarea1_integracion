564fec0817e509c1c7dc6813a00c019fafee2f9d
Lucas Rencoret <lrencore@ucsc.edu>
2016-03-15 20:29:49 -0300

    commit inicial

---
+++ controllers/default.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - api is an example of Hypermedia API support and access control
+#########################################################################
+import json
+import hashlib
+from hashlib import sha256
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Welcome to web2py!")
+    return dict(message=T('Hello World'))
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+def status():
+
+    response.status = 201
+
+    return 
+ 
+def validarFirma():
+    """
+    this is example of API with access control
+    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
+    """
+    from gluon.contrib.hypermedia import Collection
+
+    mensaje = request.post_vars.mensaje
+    mensaje_hasheado = request.post_vars.hash
+    if (mensaje==None or mensaje_hasheado==None):
+        response.status = 400
+        return dict(mensaje=mensaje, valido='false')
+    
+
+    h = sha256()
+    h.update(mensaje)
+    respuesta = h.hexdigest()
+
+    response.flash = h.hexdigest()
+    valido = True
+    if (mensaje_hasheado.lower() == respuesta):
+        valido = True
+    else:
+        valido = False
+    valido_letras = str(valido).lower()
+    return dict(mensaje=mensaje, valido=valido_letras)
+
+    
